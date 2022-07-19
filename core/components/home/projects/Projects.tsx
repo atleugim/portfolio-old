aea774af61f14b5c203ae445cf725a93fc5f252c
Miguel Vega <vegaleugim@gmail.com>
2022-07-19 18:07:15 -0500

    moved to pnpm, updated projects

--- core/components/home/projects/Projects.tsx
+++ core/components/home/projects/Projects.tsx
@@ -5,8 +5,9 @@ const Projects = () => {
   return (
     <section id="projects" className="flex flex-col w-full space-y-4">
       <p className="flex space-x-1 text-lg font-medium">
-        <span className="text-secondary">Some </span>
-        <span>things I&apos;ve built</span>
+        <span>Some</span>
+        <span className="text-secondary">things</span>
+        <span>I&apos;ve built</span>
       </p>
       <div className="flex flex-col items-start justify-start space-y-5">
         {projects.map((project) => (
--- core/utils/routes.ts
+++ core/utils/routes.ts
@@ -1,28 +1,23 @@
 import { Route } from "./types";
 
 export class Routes {
-  static home = '/';
-  static projects = '/#projects';
-  static about = '/#about';
-  static contact = '/#contact';
+  static home = "/";
+  static projects = "/#projects";
+  static contact = "/#contact";
 
   static getAll(): Array<Route> {
     return [
       {
         href: Routes.home,
-        name: 'Home',
+        name: "Home",
       },
       {
         href: Routes.projects,
-        name: 'Projects',
-      },
-      {
-        href: Routes.about,
-        name: 'About',
+        name: "Projects",
       },
       {
         href: Routes.contact,
-        name: 'Contact',
+        name: "Contact",
       },
     ];
   }
