b2dc7bf8f4ebe41f348ae39ab56b2d9ac65699d7
Miguel Vega <vegaleugim@gmail.com>
2022-11-23 12:12:49 -0500

    feat: :sparkles: added meta og tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,6 +14,34 @@ const Document = () => {
           name="keywords"
           content="developer, development, desarrollo, programación, desarrollador, react, frontend, web, web developer, flutter, dart, mobile developer, desarrollador móvil, Flutter developer, app developer, android, ios"
         />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://atleugim.vercel.app/" />
+        <meta property="og:title" content="Miguel Vega - Software Developer" />
+        <meta
+          property="og:description"
+          content="I'm a software developer based in Colombia, specialized in web and mobile development. Self-taught developer building personal digital experiences."
+        />
+        <meta
+          property="og:image"
+          content="https://atleugim.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fme.webp&w=384&q=75"
+        />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://atleugim.vercel.app/" />
+        <meta
+          property="twitter:title"
+          content="Miguel Vega - Software Developer"
+        />
+        <meta
+          property="twitter:description"
+          content="I'm a software developer based in Colombia, specialized in web and mobile development. Self-taught developer building personal digital experiences."
+        />
+        <meta
+          property="twitter:image"
+          content="https://atleugim.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fme.webp&w=384&q=75"
+        />
+
         <link rel="canonical" href="https://atleugim.vercel.app/" />
       </Head>
       <body className={process.env.IS_DEBUG ? "debug-screens" : ""}>
