1b0e9d2a5af071fde1b1439f20defeb423c798c6
Miguel Vega <vegaleugim@gmail.com>
2023-04-17 21:26:33 -0500

    feat: :sparkles: added domain email and updated contact api

--- src/core/utils/vars.ts
+++ src/core/utils/vars.ts
@@ -2,7 +2,7 @@ export const IS_DEV = process.env.NODE_ENV === "development";
 
 export const LINKEDIN_URL = "https://www.linkedin.com/in/atleugim";
 export const GITHUB_URL = "https://github.com/atleugim";
-export const EMAIL = "vegaleugim@gmail.com";
+export const EMAIL = "me@atleugim.dev";
 export const EMAIL_URL = `mailto:${EMAIL}`;
 
 export const NO_IMAGE_AVAILABLE = "/assets/images/not_found.webp";
@@ -10,7 +10,7 @@ export const NO_IMAGE_AVAILABLE = "/assets/images/not_found.webp";
 // Tech docs links
 export const JAVASCRIPT_URL =
   "https://developer.mozilla.org/en-US/docs/Web/JavaScript";
-export const REACT_URL = "https://reactjs.org/";
+export const REACT_URL = "https://react.dev/";
 export const NEXT_JS_URL = "https://nextjs.org/";
 export const TAILWIND_URL = "https://tailwindcss.com/";
 export const FLUTTER_URL = "https://flutter.dev/";
--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -4,11 +4,11 @@ import { NextApiRequest, NextApiResponse } from "next";
 const setupNodemailer = () => {
   const nodemailer = require("nodemailer");
   return nodemailer.createTransport({
-    port: 465,
-    host: "smtp.gmail.com",
+    port: process.env.SMTP_PORT,
+    host: process.env.SMTP_HOST,
     auth: {
-      user: process.env.GMAIL_EMAIL,
-      pass: process.env.GMAIL_PASSWORD,
+      user: process.env.HOSTINGER_EMAIL,
+      pass: process.env.HOSTINGER_PASSWORD,
     },
     secure: true,
   });
@@ -16,8 +16,8 @@ const setupNodemailer = () => {
 
 const send = async (data: ContactFormData) => {
   const mailData = {
-    from: data.email,
-    to: process.env.GMAIL_EMAIL,
+    from: process.env.HOSTINGER_ALIAS_EMAIL,
+    to: process.env.HOSTINGER_EMAIL,
     subject: `New portfolio message from ${data.name}`,
     html: `
       <p>Name: ${data.name}</p>
@@ -27,6 +27,15 @@ const send = async (data: ContactFormData) => {
   };
 
   const transporter = setupNodemailer();
+  // verify connection configuration
+  // transporter.verify(function (error: any, success: any) {
+  //   if (error) {
+  //     console.log(error);
+  //   } else {
+  //     console.log("Server is ready to take our messages");
+  //   }
+  // });
+
   return transporter.sendMail(mailData);
 };
 
